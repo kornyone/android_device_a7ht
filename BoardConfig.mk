02213b7500ade3ff2ec51aec3d903b553ac4e8bf
Jason Parker <kornyone@gmail.com>
2011-02-06 13:52:20 -0600

    commit of BoardConfig

---
+++ BoardConfig.mk
@@ -0,0 +1,18 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# The generic product target doesn't have any hardware-specific pieces.
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+TARGET_CPU_ABI := armeabi
+#HAVE_HTC_AUDIO_DRIVER := true
+#BOARD_USES_GENERIC_AUDIO := true
+BOARD_USES_ALSA_AUDIO := true
+BUILD_WITH_ALSA_UTILS := true
+HAVE_CUSTOM_WIFI_DRIVER_2 := true
+BUILD_WITH_FULL_STAGEFRIGHT := true
+
+# no hardware camera
+USE_CAMERA_STUB := true
